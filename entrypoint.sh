94dac9fda3eb763217f4833d475a99c0f1863bdb
Suyi <thonatos.yang@gmail.com>
2019-05-25 14:29:06 +0800

    fix: check envs

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,22 +2,22 @@
 set -e
 
 # add ssh config
-if [[ -n "$RELEASE_SSH_ID_RSA" && -n "$RELEASE_SSH_ID_RSA"]];
-then
-  mkdir -p /root/.ssh;
+if [ -n "$RELEASE_SSH_ID_RSA" ] && [ -n "$RELEASE_SSH_ID_RSA" ]; then
+  echo "# add ssh config";
 
+  mkdir -p /root/.ssh;
   echo "$RELEASE_SSH_ID_RSA" > /root/.ssh/id_rsa;
+  echo "$RELEASE_SSH_ID_RSA_PUB" > /root/.ssh/id_rsa.pub;
   chmod 400 /root/.ssh/id_rsa;
   chmod 400 /root/.ssh/id_rsa.pub;
-  echo "$RELEASE_SSH_ID_RSA_PUB" > /root/.ssh/id_rsa.pub;
-
   git config --global user.name "Release Bot";
   git config --global user.email "release_bot@eggjs.app";
 fi
 
 # add npm config
-if [[ -n "$NPM_AUTH_TOKEN" ]];
-then
+if [ -n "$NPM_AUTH_TOKEN" ]; then
+  echo "# add npm config";
+
   # Respect NPM_CONFIG_USERCONFIG if it is provided, default to $HOME/.npmrc
   NPM_CONFIG_USERCONFIG="${NPM_CONFIG_USERCONFIG-"$HOME/.npmrc"}";
   NPM_REGISTRY_URL="${NPM_REGISTRY_URL-registry.npmjs.org}";
